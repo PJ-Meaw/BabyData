9e12ee4ad66b19f942618d7a25045c609af09841
carrotmanfen <kudsun.khamjamnun@mail.kmutt.ac.th>
2022-04-17 21:18:19 +0700

    first push

---
+++ app.js
@@ -0,0 +1,77 @@
+function next_book()
+{
+    window.location = 'http://127.0.0.1:5500/-babydata/Book.html';
+}
+
+function next_home()
+{
+    window.location = 'http://127.0.0.1:5500/-babydata/Home.html';
+}
+
+function next_login()
+{
+    window.location = 'http://127.0.0.1:5500/-babydata/Login.html';
+}
+
+function next_detail()
+{
+    window.location = 'http://127.0.0.1:5500/-babydata/Details.html';
+}
+
+function next_cartroom()
+{
+    window.location = 'http://127.0.0.1:5500/-babydata/CartRoom.html';
+}
+
+function next_activity()
+{
+    window.location = 'http://127.0.0.1:5500/-babydata/Activity.html';
+}
+
+function next_food()
+{
+    window.location = 'http://127.0.0.1:5500/-babydata/Food.html';
+}
+
+function next_ourroom()
+{
+    window.location = 'http://127.0.0.1:5500/-babydata/OurRoom.html';
+}
+
+function after_login()
+{
+    var pass = document.getElementById("password").value
+    var user = document.getElementById("email-address").value
+    if (pass=="" || user=="")
+    {
+        alert("fill your E-mail and password")
+    }
+    else if(pass=="123"&&user=="123@123")
+    {
+        window.location = 'http://127.0.0.1:5500/-babydata/HomeLog.html';
+    }
+    else
+    {
+        alert("E-mail or Password Incorrect!")
+    }
+}
+
+function after_booking()
+{
+    var dayin = document.getElementById("dayin").value
+    var monthin = document.getElementById("monthin").value
+    var yearin = document.getElementById("yearin").value
+    var dayout = document.getElementById("dayout").value
+    var monthout = document.getElementById("monthout").value
+    var yearout = document.getElementById("yearout").value
+    var participant = document.getElementById("participant").value
+    if(dayin=="select"||monthin=="select"||yearin=="select"||dayout=="select"||monthout=="select"||yearout=="select"||participant=="select")
+    {
+        alert("plese select all of the box")
+    }
+    else
+    {
+        alert("asdasd")
+    }
+
+}
