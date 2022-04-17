d86b660b15483efee42176803d724861d7699dcb
Sorathorn Kaewchotchuangkul <sarathorn.16@gmail.com>
2022-04-18 01:45:24 +0700

    add

---
+++ test.js
@@ -0,0 +1,15 @@
+var num = 0;
+var str1 = "BBD";
+var str2 = num.toString();
+
+if(str2.length == 1){
+    str2 = "00" + str2;
+}
+else if(str2.length == 2){
+    str2 = "0" + str2;
+}
+
+
+console.log(str3 = str1+str2);
+count++;
+
