ce7a3223981080aaddf6cfa24014affc90e3a397
PJ-Meaw <pathinya19@gmail.com>
2022-05-03 00:50:08 +0700

    start back-end with front-end

--- meawtest.js
+++ meawtest.js
@@ -1,12 +1,17 @@
-let User_and_Promontion =  "UAP0000200000051";
-const ArraySplitNumSring = User_and_Promontion.split("P");
-let NumPromotion = parseInt(ArraySplitNumSring[1]);
-NumPromotion ++; // Add 1 for new promotion or generate
-let GenPromotion = "";
-console.log(NumPromotion);
-for(let i=0; i< 13-NumPromotion.toString().length ;i++){
-   GenPromotion += "0";
-}
-GenPromotion += NumPromotion;
+// let User_and_Promontion =  "UAP0000200000051";
+// const ArraySplitNumSring = User_and_Promontion.split("P");
+// let NumPromotion = parseInt(ArraySplitNumSring[1]);
+// NumPromotion ++; // Add 1 for new promotion or generate
+// let GenPromotion = "";
+// console.log(NumPromotion);
+// for(let i=0; i< 13-NumPromotion.toString().length ;i++){
+//    GenPromotion += "0";
+// }
+// GenPromotion += NumPromotion;
 
-console.log(GenPromotion + "");
+// console.log(GenPromotion + "");
+
+text1 = "2/5/2565"
+text2 = "2/8/2565"
+
+console.log(text1 == text2)
