ecefbfa7ac78d2129fdd7185f86a7ff353f53cb5
Sorathorn Kaewchotchuangkul <sarathorn.16@gmail.com>
2022-05-06 23:55:04 +0700

    add

--- api.js
+++ api.js
@@ -997,7 +997,7 @@ app.post('/store_promotion', jasonParser, function (req, res, next) {
                   return
                }
                var ValueOfDate_and_Room = result_date_and_room[0].date_and_room; // !!!!!!! "let"
-
+               console.log(result_date_and_room)
                db.execute(
                   'SELECT participant FROM booking WHERE booking_id = ?', // select participant from TB booking for using limit value from form  
                   [result_date_and_room[0].booking_id],
@@ -1016,6 +1016,7 @@ app.post('/store_promotion', jasonParser, function (req, res, next) {
                            return
                           }
                           
+                          
                           db.execute(
                              'SELECT activity_no FROM activity WHERE branch_no IN (SELECT branch_no FROM room WHERE room_id = ?)', //For get activity_no then calculate
                              [result_date_and_room[0].room_id],
@@ -1024,7 +1025,6 @@ app.post('/store_promotion', jasonParser, function (req, res, next) {
                                       res.json({ status: 'error', messsage: err })
                                  return
                                 }
-
                                 db.execute(
                                  'SELECT discount FROM promotion WHERE promotion_id = ?', //For get discount of promotion
                                  [req.body.promotion_id],
